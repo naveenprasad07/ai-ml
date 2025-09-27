ea593908cabde398efe11be2386974028155c431
Naveen Prasad <naveenacad89@gmail.com>
2025-09-27 11:51:03 +0530

    EDA and problem statement

--- 48-Project/src/exception.py
+++ 48-Project/src/exception.py
@@ -1,5 +1,8 @@
 import sys
-import logging
+import os 
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
